808009665109e4f0a2b080d522bb6aed10dbaced
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-09 08:10:44 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -2,14 +2,14 @@
 
 class Book
 
-    attr_accessor :title, :page_count, :author, :genre
+    attr_accessor :title, :page_count, :author, :genre :turn_page
 
     def initialize (title= "And Then There Were None")
         @title = title
     end
 
-    def turn_page (turn_page)
-        @turn_page= turn_page
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
     end
 end
 
